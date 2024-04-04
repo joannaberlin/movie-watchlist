c6ac7c2d95fdf5c2ec216d9979dc8f9ec0cbd2c2
joannaberlin <joanna.kot.ok@gmail.com>
2024-04-04 19:22:07 +0200

    Render movies from watch list from local storage in watchlist page

--- watchlist.js
+++ watchlist.js
@@ -1,3 +1,24 @@
-//get data saved in local storage on the movies list page
-//render that data as 'my watchlist'
-let movies = [];
+const watchListContainer = document.getElementById('watch-list_container');
+const emptyListContainer = document.getElementById('emptyList_wrapper');
+
+let movies = localStorage.getItem('movies')
+	? JSON.parse(localStorage.getItem('movies'))
+	: [];
+
+console.log(movies);
+
+for (let movie of movies) {
+	emptyListContainer.classList.add('hide');
+	watchListContainer.innerHTML += `
+        <div class="img-item">
+            <img src=${movie.posterUrl} alt="poster for ${movie.title}"/>
+            <div class="desc">
+                <h3>${movie.title} <span id="rating-for-${movie.id}">⭐ ${movie.rating}</span></h3>
+                <h5>${movie.genre}</h5>
+                <p>${movie.plot}</p>
+                <button id="${movie.id}"><span class="add-btn">-</span>Remove</button>
+            </div>
+        </div>
+        <hr/>
+        `;
+}
